c58cf11e2bed1f41cd39f8c054026d88296b826e
Alex Wu <adwu@pm.me>
2023-11-14 22:41:23 -0800

    gets rid of some scheduling and fixes leading text offset calculation

--- lua/nucleo/entry.lua
+++ lua/nucleo/entry.lua
@@ -1,10 +1,7 @@
 local Entry = require("plenary.class"):extend()
 local Line = require("nucleo.line")
 local Text = require("nui.text")
-local log = require("nucleo.log")
-local strdisplaywidth = require("plenary.strings").strdisplaywidth
 
-local ns_selection = vim.api.nvim_create_namespace("nucleo_selection")
 local ns_matching = vim.api.nvim_create_namespace("nucleo_matching")
 
 function Entry:new(index, entry, bufnr)
@@ -27,26 +24,24 @@ end
 
 ---@return string
 function Entry:render()
+	-- TODO: Make the separator a part of the Line API
 	local picker_icon = Text(self.picker_icon, "Comment")
 	local icon = Text(self.icon.value, self.icon.color)
 	local path = Text(self.entry.path)
 	local line = Line({ picker_icon, icon, path })
 
-	-- TODO: Make the separator a part of the Line API
-	local leading_length = strdisplaywidth(self.icon.value) + strdisplaywidth(self.picker_icon) + strdisplaywidth("  ")
+	local leading_length = picker_icon:length() + icon:length()
 
 	line:render(self.bufnr, -1, self.index, -1)
 	vim.iter(self.entry.indices):each(function(range)
-		vim.schedule(function()
-			vim.highlight.range(
-				self.bufnr,
-				ns_matching,
-				"TelescopeMatching",
-				{ self.index - 1, leading_length + 1 + range[1] + 1 },
-				{ self.index - 1, leading_length + 1 + range[2] + 1 },
-				{ inclusive = true }
-			)
-		end)
+		vim.highlight.range(
+			self.bufnr,
+			ns_matching,
+			"TelescopeMatching",
+			{ self.index - 1, leading_length + 1 + range[1] + 1 },
+			{ self.index - 1, leading_length + 1 + range[2] + 1 },
+			{ inclusive = true }
+		)
 	end)
 end
 
