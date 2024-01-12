9f54321d34e09edd861438a213b281ce99ea8481
Alex Wu <adwu@pm.me>
2024-01-11 21:03:59 -0800

    Custom error type

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,13 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    Lua(#[from] mlua::Error),
+    #[error("{0}")]
+    TryRecvError(#[from] crossbeam_channel::TryRecvError),
+    #[error("{0}")]
+    Git(#[from] git2::Error),
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
+}
 
+pub type Result<T> = core::result::Result<T, Error>;
