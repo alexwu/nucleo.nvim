cd3b4662fcaa0ec0224a6814ddb7c8a0d37000ab
Alex Wu <adwu@pm.me>
2024-01-11 18:55:44 -0800

    Tweaks presets

--- lua/nucleo/presets.lua
+++ lua/nucleo/presets.lua
@@ -9,23 +9,41 @@ function M.horizontal()
 				relative = "editor",
 				position = "50%",
 				size = {
-					width = "80%",
-					height = "80%",
+					width = "90%",
+					height = "90%",
 				},
 			},
 			Layout.Box({
 				Layout.Box({
-					Layout.Box(prompt, { size = { width = "100%", height = 3 } }),
+					Layout.Box(prompt, { size = 3 }),
 					Layout.Box(results, { grow = 1 }),
-				}, { dir = "col", size = "50%" }),
+				}, { dir = "col", grow = 1 }),
 				Layout.Box({
-					Layout.Box(previewer, { size = "100%" }),
-				}, { dir = "col", size = "50%" }),
+					Layout.Box(previewer, { grow = 1 }),
+				}, { dir = "col", grow = 1 }),
 			}, { dir = "row" })
 		)
 	end
 end
 
-function M.center() end
+function M.center()
+	return function(prompt, results, previewer)
+		return Layout(
+			{
+				relative = "editor",
+				position = "50%",
+				size = {
+					width = "50%",
+					height = "90%",
+				},
+			},
+			Layout.Box({
+				Layout.Box(results, { size = "50%" }),
+				Layout.Box(prompt, { size = { width = "100%", height = 3 } }),
+				Layout.Box(previewer, { grow = 1 }),
+			}, { dir = "col", size = "100%" })
+		)
+	end
+end
 
 return M
